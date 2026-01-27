715a76cf14acd8299a15d889b95fe7ef1652a24a
Cezar Dias <cezar.dias@gmail.com>
2026-01-27 18:53:22 -0300

    fix: resolve nestjs dependency deadlock and boot failure in users module

--- backend-core/src/users/users.module.ts
+++ backend-core/src/users/users.module.ts
@@ -5,11 +5,13 @@ import { Tenant } from './entities/tenant.entity';
 import { UsersService } from './users.service';
 import { AdminController } from './admin.controller';
 import { CrmModule } from '../crm/crm.module';
+import { SupportModule } from '../support/support.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User, Tenant]),
         forwardRef(() => CrmModule),
+        SupportModule,
     ],
     controllers: [AdminController],
     providers: [UsersService],
