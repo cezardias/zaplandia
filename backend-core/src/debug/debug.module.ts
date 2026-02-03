9c06488085add48a502c19e43a651bdc0ddc7ac3
Cezar Dias <cezar.dias@gmail.com>
2026-02-03 00:25:28 -0300

    fix(debug): add IntegrationsModule to DebugModule

--- backend-core/src/debug/debug.module.ts
+++ backend-core/src/debug/debug.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DebugController } from './debug.controller';
 import { Contact } from '../crm/entities/crm.entity';
+import { Integration } from '../integrations/entities/integration.entity';
+import { IntegrationsModule } from '../integrations/integrations.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Contact])],
+    imports: [
+        TypeOrmModule.forFeature([Contact, Integration]),
+        IntegrationsModule
+    ],
     controllers: [DebugController],
 })
 export class DebugModule { }
