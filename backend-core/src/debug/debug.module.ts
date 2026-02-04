aa9ac2f324a241e588beceb975c934a6f3fd97ab
Cezar Dias <cezar.dias@gmail.com>
2026-02-04 15:16:26 -0300

    Fix: Import in DebugModule and final adjustments

--- backend-core/src/debug/debug.module.ts
+++ backend-core/src/debug/debug.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DebugController } from './debug.controller';
-import { Contact } from '../crm/entities/crm.entity';
-import { Message } from '../messages/entities/message.entity'; // Added import for Message entity
+import { Contact, Message } from '../crm/entities/crm.entity';
 import { Integration } from '../integrations/entities/integration.entity';
 import { IntegrationsModule } from '../integrations/integrations.module';
 
