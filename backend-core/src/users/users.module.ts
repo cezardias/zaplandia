7801c4aa2d97510c0141e74866a3b0dda9b0b867
Cezar Dias <cezar.dias@gmail.com>
2026-02-05 14:16:12 -0300

    feat: Add auto-cleanup service to remove all non-superadmin data on startup

--- backend-core/src/users/users.module.ts
+++ backend-core/src/users/users.module.ts
@@ -4,25 +4,20 @@ import { User } from './entities/user.entity';
 import { Tenant } from './entities/tenant.entity';
 import { UsersService } from './users.service';
 import { AdminController } from './admin.controller';
-import { TenantMigrationService } from './tenant-migration.service';
-import { TenantMigrationController } from './tenant-migration.controller';
+import { DatabaseCleanupService } from './database-cleanup.service';
 import { CrmModule } from '../crm/crm.module';
 import { SupportModule } from '../support/support.module';
 import { IntegrationsModule } from '../integrations/integrations.module';
-import { Contact } from '../crm/entities/crm.entity';
-import { Message } from '../crm/entities/message.entity';
-import { Integration } from '../integrations/entities/integration.entity';
-import { Campaign } from '../campaigns/entities/campaign.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User, Tenant, Contact, Message, Integration, Campaign]),
+        TypeOrmModule.forFeature([User, Tenant]),
         forwardRef(() => CrmModule),
         SupportModule,
         IntegrationsModule,
     ],
-    controllers: [AdminController, TenantMigrationController],
-    providers: [UsersService, TenantMigrationService],
+    controllers: [AdminController],
+    providers: [UsersService, DatabaseCleanupService],
     exports: [UsersService],
 })
 export class UsersModule { }
