443d1a03439f6576497d6fbcd302b1a3e6d6af56
Cezar Dias <cezar.dias@gmail.com>
2026-04-14 13:53:29 -0300

    fix: add missing InternalServerErrorException import in BillingService

--- backend-core/src/billing/billing.service.ts
+++ backend-core/src/billing/billing.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Tenant } from '../users/entities/tenant.entity';
