b1f07423774c5a0d00fdfd65a5d2e9115515e98c
Cezar Dias <cezar.dias@gmail.com>
2026-04-23 12:46:16 -0300

    fix(auth): remove openid from scope to match advanced access permissions

--- backend-core/src/auth/strategies/facebook.strategy.ts
+++ backend-core/src/auth/strategies/facebook.strategy.ts
@@ -9,7 +9,7 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
             clientID: process.env.FACEBOOK_APP_ID || '760305269821467',
             clientSecret: process.env.FACEBOOK_APP_SECRET || '6685a0cba55f2f49349222565cf37042',
             callbackURL: 'https://zaplandia.com.br/api/auth/facebook/callback',
-            scope: ['email', 'public_profile', 'openid'],
+            scope: ['email', 'public_profile'],
             profileFields: ['id', 'emails', 'name', 'photos'],
             graphAPIVersion: 'v18.0',
         });
